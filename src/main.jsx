a9ec0006e475e7f853cc8d491a4192db16631652
Claudy <128431348+Klaudy-B@users.noreply.github.com>
2023-06-19 09:57:27 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import AppWrapper from './AppWrapper.jsx'
+import AppWrapper from './AppWrapper'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
