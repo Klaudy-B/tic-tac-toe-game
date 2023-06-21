09494e7e5274c40d65ad0f41f0ab0e573284de9c
Klaudy-B <claudyaddress@gmail.com>
2023-06-21 15:11:42 -0700

    set base url and build.outDir in confing file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
+  base: 'https://klaudy-b.github.io/tic-tac-toe-game/',
 })
